59ccbbeceb16b2d6377a61b851aad4a74547239d
yuta3817 <yuta2213817@gmail.com>
2021-01-19 22:10:09 +0900

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -2,8 +2,8 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
@@ -12,12 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_01_15_131556) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "cards", force: :cascade do |t|
     t.date "date", null: false
-    t.bigint "list_id", null: false
+    t.integer "list_id", null: false
     t.string "title", limit: 255, null: false
     t.text "memo", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -27,7 +24,7 @@ ActiveRecord::Schema.define(version: 2021_01_15_131556) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title", null: false
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_lists_on_user_id"
