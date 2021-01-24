1ba3024561463a3149ccccb17e3a582ad674a42f
yuta3817 <yuta2213817@gmail.com>
2021-01-25 00:38:24 +0900

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -2,8 +2,8 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
@@ -12,19 +12,17 @@
 
 ActiveRecord::Schema.define(version: 2021_01_15_131556) do
 
-  # These are extensions that must be enabled in order to support this database
-
-  create_table "cards", force: :cascade do |t|
+  create_table "cards", charset: "utf8", force: :cascade do |t|
     t.date "date", null: false
     t.bigint "list_id", null: false
-    t.string "title", limit: 255, null: false
+    t.string "title", null: false
     t.text "memo", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["list_id"], name: "index_cards_on_list_id"
   end
 
-  create_table "lists", force: :cascade do |t|
+  create_table "lists", charset: "utf8", force: :cascade do |t|
     t.string "title", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -32,7 +30,7 @@ ActiveRecord::Schema.define(version: 2021_01_15_131556) do
     t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
