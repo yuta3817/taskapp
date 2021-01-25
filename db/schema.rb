2a71bbe7ecd0d5ebcf60cc94408ae0a816e2d5e4
yuta3817 <yuta2213817@gmail.com>
2021-01-25 18:34:37 +0900

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,8 @@ ActiveRecord::Schema.define(version: 2021_01_25_053741) do
 
   create_table "cards", charset: "utf8", force: :cascade do |t|
     t.date "date", null: false
-    t.string "title", null: false
+    t.integer "list_id", null: false
+    t.string "title", limit: 255, null: false
     t.text "memo", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -23,8 +24,7 @@ ActiveRecord::Schema.define(version: 2021_01_25_053741) do
 
   create_table "lists", charset: "utf8", force: :cascade do |t|
     t.string "title", null: false
-    t.text "memo"
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_lists_on_user_id"
