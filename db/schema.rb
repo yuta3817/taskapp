73ebb907efe012808b2e5456903da51aa693e130
yuta3817 <yuta2213817@gmail.com>
2021-01-25 14:23:38 +0900

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(version: 2021_01_15_131556) do
   end
 
   create_table "lists", charset: "utf8", force: :cascade do |t|
-    t.string "title", null: false
-    t.bigint "user_id", null: false
+    t.string "title", limit: 255, null: false
+    t.text "memo", limit: 1000
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_lists_on_user_id"
